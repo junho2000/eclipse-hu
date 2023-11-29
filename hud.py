ef360a74100d678c7386352c35f9513a8859c758
junho <masterjunho@gmail.com>
2023-11-29 09:26:16 +0100

    add image

--- hud.py
+++ hud.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 import pymysql
 import time
-
+from PIL import ImageTk, Image
 class HUDApp(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -18,22 +18,29 @@ class HUDApp(tk.Tk):
         )
 
         self.is_mysql_connected()
+        
+        # Load and display the image
+        original_image = Image.open('/Users/kimjunho/Downloads/channels4_profile.png')
+        resized_image = original_image.resize((200, 200), Image.BICUBIC)  # Set 'width' and 'height' to your desired values
+        self.image = ImageTk.PhotoImage(resized_image)
+        self.image_label = tk.Label(self, image=self.image)
+        self.image_label.pack()
 
         # Create labels
-        self.speed_label = tk.Label(self, text="Speed: 0 km/h", font=("Helvetica", 40))
-        self.speed_label.pack()
+        self.speed_label = tk.Label(self, text="Speed: 0 km/h", font=("Helvetica", 60))
+        self.speed_label.pack(anchor='center')
 
-        self.battery_label = tk.Label(self, text="Battery: 0 %", font=("Helvetica", 40))
-        self.battery_label.pack()
+        self.battery_label = tk.Label(self, text="Battery: 0 %", font=("Helvetica", 60))
+        self.battery_label.pack(anchor='center')
 
-        self.temp_label = tk.Label(self, text="Motor Temp: 0 °C", font=("Helvetica", 40))
-        self.temp_label.pack()
+        self.temp_label = tk.Label(self, text="Motor Temp: 0 °C", font=("Helvetica", 60))
+        self.temp_label.pack(anchor='center')
 
-        self.air_label = tk.Label(self, text="Air Conditioner: Off", font=("Helvetica", 40))
-        self.air_label.pack()
+        self.air_label = tk.Label(self, text="Air Conditioner: Off", font=("Helvetica", 60))
+        self.air_label.pack(anchor='center')
 
-        self.window_label = tk.Label(self, text="Window Open: 0 %", font=("Helvetica", 40))
-        self.window_label.pack()
+        self.window_label = tk.Label(self, text="Window Open: 0 %", font=("Helvetica", 60))
+        self.window_label.pack(anchor='center')
 
         # Start the periodic update
         self.update_data()
